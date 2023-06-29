d27b62fa52c8fc51917c38cb52e099c0af194a22
dsw49512 <dsw49512@student.dsw.edu.pl>
2023-06-29 18:54:44 +0200

    stworzylem error

--- skrypt.sh
+++ skrypt.sh
@@ -18,6 +18,23 @@ if [[ "$1" == "--logs" || "$1" == "-l" ]]; then
     echo "Data: $(date)" >> "$filename"
   done
 fi
+
+if [[ "$1" == "--error" || "$1" == "-e" ]]; then
+        if [ -n "$2" ]; then
+                count="$2"
+        else
+                count=100
+         fi
+ for ((i=1; i<=count; i++))
+  do
+    dir = "error${i}"
+    mkdir -p "$dir"
+    filename="${dir}/error{i}.txt"
+    echo "Nazwa pliku: $filename" >> "$filename"
+    echo "Nazwa skryptu: skrypt.sh" >> "$filename"
+    echo "Data: $(date)" >> "$filename"
+  done
+fi
 if [[ "$1" == "--init" || "$1" == "-i" ]]; then
 	git clone https://github.com/dsw49512/lab4
 	export PATH = "$PWD:$PATH"
@@ -27,6 +44,7 @@ fi
 if [[ "$1" == "--help" || "$1" == "-h" ]]; then
 	echo "Wybierz funkcje"
 	echo "--logs/-l (liczba)"
+	echo "--error/-e (liczba)"
 	echo "--date/-d"
 else 
 	echo "Nie prawidłowa opcja, wpisz --help/-h"
